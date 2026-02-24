0bf672958b45c1b2f4e595f087acc2964c746db8
JonEBeck <jonathan@mdbeck.com>
2026-02-24 15:18:12 -0600

    UI redesigned to be easier to read.

--- blackjack.py
+++ blackjack.py
@@ -7,12 +7,15 @@ deck = ["A","A","A","A",2,2,2,2,3,3,3,3,4,4,4,4,5,5,5,5,6,6,6,6,7,7,7,7,
 mult = 1
 buy_in = -1
 double = 1
+double1 = 1
 player_hand = []
 player_hand1 = []
 hands = [player_hand]
 dealer_hand = []
 bust = False
 blackjack = False
+split = "n"
+player_turn = 1
 
 #This initializes the doubloons value from the file
 with open('/usr/local/bin/Money', 'r') as file:
@@ -22,6 +25,12 @@ with open('/usr/local/bin/Money', 'r') as file:
 def write(total):
     with open('/usr/local/bin/Money', 'w') as file1:
         file1.write(f"{total}")
+
+#Prints letter by letter.
+def slow_print(string):
+    for character in string:
+        print(character, end="", flush=True)
+        time.sleep(.05)
 
 def wait():
     time.sleep(1.2)
@@ -76,37 +85,70 @@ def draw(which_hand):
     card = random.choice(deck)
     which_hand.append(card)
     deck.remove(card)
-    if which_hand == player_hand or which_hand == player_hand1:
-        print("")
-        print(f"You drew {card}")
 
 #This function prints the information that should be available to the player about both hands
 def print_hands():
-    if len(hands) > 1:
-        count_words = ["first", "second"]
-        for hand in hands:
-            wait()
-            print("")
-            print(f"Your {str(count_words[0])} hand is {pretty(hand)}, totaling {math(hand)}.")
-            count_words.pop(0)
-        wait()
+    os.system("clear")
+    print(f"Purse: {doubloons} doubloons")
+    print("")
+    if split.lower() == "n":
+        print(f"You have {buy_in * double} doubloons on this game")
+        print("------------------------------------------------------------------")
         print("")
-        print(f"The dealer has {str(dealer_hand[0])} and an unrevealed card.")
     else:
+        print(f"You have {buy_in * double + buy_in * double1} doubloons on this game. split")
+        print("------------------------------------------------------------------")
         print("")
-        print(f"Your hand is {pretty(player_hand)}, totaling {math(player_hand)}.")
-        wait()
-        print("")
-        print(f"The dealer has {str(dealer_hand[0])} and an unrevealed card.")
+    if player_turn == 1:
+        if len(hands) > 1:
+            count_words = ["first", "second"]
+            for hand in hands:
+                print("")
+                print("")
+                slow_print(f"Your {str(count_words[0])} hand is {pretty(hand)}, totaling {math(hand)}.")
+                count_words.pop(0)
+            print("")
+            print("")
+            slow_print(f"The dealer has {str(dealer_hand[0])} and an unrevealed card.")
+        else:
+            print("")
+            print("")
+            slow_print(f"Your hand is {pretty(player_hand)}, totaling {math(player_hand)}.")
+            print("")
+            print("")
+            slow_print(f"The dealer has {str(dealer_hand[0])} and an unrevealed card.")
+    else:
+        if len(hands) > 1:
+            count_words = ["first", "second"]
+            for hand in hands:
+                print("")
+                print("")
+                slow_print(f"Your {str(count_words[0])} hand is {pretty(hand)}, totaling {math(hand)}.")
+                count_words.pop(0)
+            print("")
+            print("")
+            slow_print(f"The dealer has {pretty(dealer_hand)}, totaling {math(dealer_hand)}.")
+            wait()
+        else:
+            print("")
+            print("")
+            slow_print(f"Your hand is {pretty(player_hand)}, totaling {math(player_hand)}.")
+            print("")
+            print("")
+            slow_print(f"The dealer has {pretty(dealer_hand)}, totaling {math(dealer_hand)}.")
+            wait()
+
 def win():
     global mult
     global buy_in
     print("")
-    print(f"You won {int(mult * double * buy_in)} doubloons!")
+    print("")
+    slow_print(f"You won {int(mult * double * buy_in)} doubloons!")
 def lose():
     global buy_in
     print("")
-    print(f"You lost {int(double * buy_in)} doubloons...")
+    print("")
+    slow_print(f"You lost {int(double * buy_in)} doubloons...")
 
 #This is the main logic circuit of the game
 def game():
@@ -114,22 +156,27 @@ def game():
     global buy_in
     global mult
     global double
+    global double1
     global hands
     global player_hand
     global player_hand1
+    global split
+    global player_turn
     player_blackjack = False
     player_blackjack1 = False
+    player_turn = 1
     double = 1
     double1 = 1
     buy_in = -1
     split = "n"
-    print(f"You have {doubloons} doubloons.")
-    wait()
+    slow_print(f"You have {doubloons} doubloons.")
     while buy_in < 0 or buy_in > doubloons:
         print("")
-        buy_in = int(input("How many doubloons do you throw in?:"))
+        print("")
+        slow_print("How many doubloons do you throw in?:")
+        buy_in = int(input())
     doubloons = doubloons - buy_in
-    wait()
+    write(doubloons)
     initialize(player_hand)
     initialize(dealer_hand)
     print_hands()
@@ -139,12 +186,14 @@ def game():
     elif math(dealer_hand) == 21:
         hit = "s"
     elif val(player_hand[0]) == val(player_hand[1]):
-        wait()
         print("")
-        split = input("Would you like to split? (y/n):")
+        print("")
+        slow_print("Would you like to split? (y/n):")
+        split = input()
         hit = "h"
         if split.lower() == "y" and int(doubloons - buy_in > -1):
             doubloons -= buy_in
+            write(doubloons)
             player_hand1.append(player_hand[1])
             player_hand.pop(1)
             hands.append(player_hand1)
@@ -162,160 +211,171 @@ def game():
                     else:
                         player_blackjack1 = True
         elif split.lower() == "y":
-            wait()
             print('')
-            print("You momentarily considered throwing in your watch...")
+            print("")
+            slow_print("You momentarily considered throwing in your watch...")
             wait()
             wait()
             wait()
             print("")
-            print("No. Maybe another day.")
+            print("")
+            slow_print("No. Maybe another day.")
     else:
         hit = "h"
     for which_hand in hands:
         counter = 0
         while math(which_hand) < 21 and hit.lower() == "h":
-            wait()
             print("")
-            hit = input("Would you like to hit, stand, or double down? (h,s,d):")
+            print("")
+            slow_print("Would you like to hit, stand, or double down? (h,s,d):")
+            hit = input()
             while hit.lower() != "h" and hit.lower() != "s" and hit.lower() != "d":
-                hit = input("INCORRECT INPUT! Would you like to hit, stand, or double down? (h,s,d):")
+                print("")
+                print("")
+                slow_print("INCORRECT INPUT! Would you like to hit, stand, or double down? (h,s,d):")
+                hit = input()
             while hit.lower() == "d" and doubloons - buy_in < 0:
+                print("")
+                print("")
+                slow_print("You momentarily consider putting your car on the game...")
+                wait()
+                wait()
                 wait()
                 print("")
-                print("You momentarily consider putting your car on the game...")
-                wait()
-                wait()
-                wait()
                 print("")
-                print("No. It's not worth it.")
-                hit = input("Would you like to hit, stand, or double down? (h,s,d):")
+                slow_print("No. It's not worth it.")
+                print("")
+                print("")
+                slow_print("Would you like to hit, stand, or double down? (h,s,d):")
+                hit = input()
             if hit.lower() == "h":
-                wait()
                 draw(which_hand)
                 print_hands()
                 counter += 1
             elif hit.lower() == "d" and counter == 0:
-                wait()
                 print("")
-                print(f"You toss in {buy_in} more doubloons...")
+                print("")
+                slow_print(f"You toss in {buy_in} more doubloons...")
                 draw(which_hand)
-                print_hands()
                 if which_hand == player_hand:
                     double = 2
                 else:
                     double1 = 2
                 doubloons -= buy_in
+                write(doubloons)
+                print_hands()
             elif hit.lower() == "d" and counter != 0:
-                wait()
                 print("")
-                print("You can't double down after you hit!")
                 print("")
-                hit = input("Would you like to hit, stand, or double down? (h,s,d):")
+                slow_print("You can't double down after you hit!")
+                print("")
+                print("")
+                slow_print("Would you like to hit, stand, or double down? (h,s,d):")
+                hit = input()
         hit = "h"
 
 #This begins the Dealer's turn
     for which_hand in hands:
         if math(dealer_hand) < 21 and math(which_hand) < 22:
+            player_turn = 0
             if not (player_blackjack and player_blackjack1) and split == "y":
                 while math(dealer_hand) < 17:
-                    wait()
-                    print("")
-                    print(f"The dealer reveals {pretty(dealer_hand)}, totaling {math(dealer_hand)}.")
+                    print_hands()
                     draw(dealer_hand)
-                wait()
                 print("")
-                print(f"The dealer reveals {pretty(dealer_hand)}, totaling {math(dealer_hand)}.")
-                wait()
+                print("")
+                slow_print(f"The dealer reveals {pretty(dealer_hand)}, totaling {math(dealer_hand)}.")
                 if math(which_hand) > math(dealer_hand):
                     print("")
-                    print("The player wins!")
-                    wait()
+                    print("")
+                    slow_print("The player wins!")
                     mult = 2
                     win()
                 elif math(dealer_hand) > 21:
                     print("")
-                    print("The dealer busts.")
-                    wait()
+                    print("")
+                    slow_print("The dealer busts.")
                     mult = 2
                     win()
                 elif math(dealer_hand) > math(which_hand):
                     print("")
-                    print("The dealer wins.")
-                    wait()
+                    print("")
+                    slow_print("The dealer wins.")
                     mult = 0
                     lose()
                 else:
                     print("")
-                    print("The hand is a push.")
-                    wait()
+                    print("")
+                    slow_print("The hand is a push.")
                     mult = 1
-                    print("You'll keep your doubloons to gamble another day!")
+                    print("")
+                    print("")
+                    slow_print("You'll keep your doubloons to gamble another day!")
             elif not player_blackjack:
                 while math(dealer_hand) < 17:
-                    wait()
-                    print("")
-                    print(f"The dealer reveals {pretty(dealer_hand)}, totaling {math(dealer_hand)}.")
+                    print_hands()
                     draw(dealer_hand)
-                wait()
-                print("")
-                print(f"The dealer reveals {pretty(dealer_hand)}, totaling {math(dealer_hand)}.")
-                wait()
+                print_hands()
                 if math(which_hand) > math(dealer_hand):
                     print("")
-                    print("The player wins!")
-                    wait()
+                    print("")
+                    slow_print("The player wins!")
                     mult = 2
                     win()
                 elif math(dealer_hand) > 21:
                     print("")
-                    print("The dealer busts.")
-                    wait()
+                    print("")
+                    slow_print("The dealer busts.")
                     mult = 2
                     win()
                 elif math(dealer_hand) > math(which_hand):
                     print("")
-                    print("The dealer wins.")
-                    wait()
+                    print("")
+                    slow_print("The dealer wins.")
                     mult = 0
                     lose()
                 else:
                     print("")
-                    print("The hand is a push.")
-                    wait()
+                    print("")
+                    slow_print("The hand is a push.")
                     mult = 1
-                    print("You'll keep your doubloons to gamble another day!")
+                    print("")
+                    print("")
+                    slow_print("You'll keep your doubloons to gamble another day!")
 
 
             else:
                 print("")
-                print(f"The dealer reveals {pretty(dealer_hand)}, totaling {math(dealer_hand)}.")
                 print("")
-                print("The player wins with a blackjack!")
-                wait()
+                slow_print(f"The dealer reveals {pretty(dealer_hand)}, totaling {math(dealer_hand)}.")
+                print("")
+                print("")
+                slow_print("The player wins with a blackjack!")
                 mult = 2.5
                 win()
 
         elif math(dealer_hand) == 21 and math(which_hand) == 21:
             print("")
-            print(f"The dealer reveals {pretty(dealer_hand)}, totaling {math(dealer_hand)}.")
             print("")
-            print("The hand is a push.")
-            wait()
+            slow_print(f"The dealer reveals {pretty(dealer_hand)}, totaling {math(dealer_hand)}.")
+            print("")
+            print("")
+            slow_print("The hand is a push.")
             mult = 1
             print("You'll keep your doubloons to gamble another day!")
         elif math(which_hand) > 21:
             print("")
-            print("The player busts.")
-            wait()
+            print("")
+            slow_print("The player busts.")
             mult = 0
             lose()
         else:
             print("")
-            print(f"The dealer reveals {pretty(dealer_hand)}, totaling {math(dealer_hand)}.")
             print("")
-            print("The dealer wins.")
-            wait()
+            slow_print(f"The dealer reveals {pretty(dealer_hand)}, totaling {math(dealer_hand)}.")
+            print("")
+            print("")
+            slow_print("The dealer wins.")
             mult = 0
             lose()
         if which_hand == player_hand:
@@ -327,9 +387,12 @@ def game():
 
 
 #This is where the actual code runs
-play = input("Would you like to play Blackjack? (y/n):")
+slow_print("Would you like to play Blackjack? (y/n):")
+play = input()
 if play.lower() == "y":
-    rules = input("Do you know the rules? (y/n):")
+    print("")
+    slow_print("Do you know the rules? (y/n):")
+    rules = input()
     if rules.lower() != "y":
         cont = "n"
         print('''
@@ -369,8 +432,10 @@ if play.lower() == "y":
         dealer_hand = []
         os.system('clear')
         game()
-        print("""""")
-        print(f"Your purse now contains {doubloons} doubloons.")
-        wait()
         print("")
-        play = input("Would you like to play Blackjack again? (y/n):")
+        print("")
+        slow_print(f"Your purse now contains {doubloons} doubloons.")
+        print("")
+        print("")
+        slow_print("Would you like to play Blackjack again? (y/n):")
+        play = input()
